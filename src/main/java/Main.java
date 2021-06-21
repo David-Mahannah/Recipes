561ea9e76fcdd819da8bc170113ab5b785dc2b59
David <mahannahd@gmail.com>
2021-06-20 20:56:00 -0500

    Added test print

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main (String[] args)
     {
         System.out.println("hello");
-
+        System.out.println("This is a test commit");
         MongoClient mongoClient = MongoClients.create(
                 "mongodb://Username:Password@cluster0-shard-00-00.zs3y6.mongodb.net:27017,cluster0-shard-00-01.zs3y6.mongodb.net:27017,cluster0-shard-00-02.zs3y6.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-d8sbv8-shard-0&authSource=admin&retryWrites=true&w=majority");
         MongoDatabase database = mongoClient.getDatabase("test");
