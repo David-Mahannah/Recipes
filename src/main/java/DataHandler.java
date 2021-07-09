6f31562b7344e3bee89de92ed32fddde7c97b6bd
David <mahannahd@gmail.com>
2021-07-09 06:56:17 -0500

    Adding additional methods to DataHandler...

--- src/main/java/DataHandler.java
+++ src/main/java/DataHandler.java
@@ -1,7 +1,6 @@
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 import org.bson.conversions.Bson;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -33,22 +32,46 @@ public class DataHandler
         return list.get(rand.nextInt(list.size()));
     }
 
-    //
-    public List<Document> findRecipeByName(String s)
+    public List<Document> findRecipeByName(String search_string)
     {
-        Bson filter = eq("recipe", s);
+        Bson filter = eq("recipe", search_string);
         List<Document> list = collection.find().into(new ArrayList<>());
         return list;
     }
 
-    public Document containsIngredient()
+    public List<Document> containsIngredient(String search_string)
     {
         Bson filter = ne("", "");
         return null;
     }
 
-    public Document doesNotContainIngredient()
+    public List<Document> doesNotContainIngredient(String search_string)
     {
         return null;
     }
+
+    public List<Document> fromRegion(String region)
+    {
+        Bson filter = eq("region", region);
+        List<Document> list = collection.find().into(new ArrayList<>());
+        return list;
+    }
+
+    // Returns List<Document> of all recipes that contain flavor
+    public List<Document> isFlavor(String flavor)
+    {
+        Bson filter = eq("flavor", flavor);
+        List<Document> list = collection.find().into(new ArrayList<>());
+        return list;
+    }
+
+    // Returns List<Documents> of all recipes that contain all flavors
+    public List<Document> hasFlavors(String... flavors)
+    {
+        Bson filter = eq("flavor", flavors);
+        List<Document> list = collection.find().into(new ArrayList<>());
+        return list;
+    }
+
+
 }
